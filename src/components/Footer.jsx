d00e9b31625e0e858c8d0e8114f277ce4628c43f
zeinab <zeinabhteit1@gmail.com>
2025-02-17 21:11:26 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import "../styles/Footer.css";
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function Footer() {
   return (
@@ -13,11 +14,11 @@ function Footer() {
       <div className="links">
         <h3>LINKS</h3>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Project</a></li>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Production</a></li>
+        <li><Link to="/">Home</Link></li> {/* Link to Home page */}
+          <li><Link to="/projects">Projects</Link></li> {/* Link to Project page */}
+          <li><Link to="/contact">Contact Us</Link></li> {/* Link to Contact Us page */}
+          <li><Link to="/about">About Us</Link></li> {/* Link to About Us page */}
+          <li><Link to="/production">Production</Link></li> {/* Link to Production page */}
         </ul>
       </div>
 
